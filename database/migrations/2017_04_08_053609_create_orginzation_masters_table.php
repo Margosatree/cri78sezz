32a16c7b6c95c8c47fc069d8eb8144a1d6bff30d
nikhil-joshi-margosatree <nikhil@margosatree.com>
2017-04-08 12:38:05 +0530

    Added Database Detail

---
+++ database/migrations/2017_04_08_053609_create_orginzation_masters_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrginzationMastersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+     public function up()
+     {
+       Schema::create('organization_masters', function (Blueprint $table) {
+         $table->increments('id');
+         $table->string('name');
+         $table->string('address');
+         $table->string('landmark')->nullable();
+         $table->string('city');
+         $table->string('state');
+         $table->string('country');
+         $table->integer('pincode');
+         $table->string('business_type')->nullable();
+         $table->string('business_operation');
+         $table->string('spoc');
+         $table->timestamps();
+       });
+     }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+     public function down()
+     {
+         Schema::dropIfExists('organization_masters');
+     }
+}
