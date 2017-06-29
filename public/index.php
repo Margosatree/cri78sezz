20c32b736886d6d984e694cb8b93179ae287f466
BrijeshWebDevNew <brijesh.dubey@margosatree.com>
2017-06-29 13:59:38 +0530

    Header add in index.php

--- public/index.php
+++ public/index.php
@@ -6,6 +6,9 @@
  * @package  Laravel
  * @author   Taylor Otwell <taylor@laravel.com>
  */
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: Authorization,token,X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
 
 /*
 |--------------------------------------------------------------------------
