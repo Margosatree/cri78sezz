1ae5a330b4ddecb782014c9cf386ea84cf0e2540
BrijeshWebDevNew <brijesh.dubey@margosatree.com>
2017-07-05 18:59:57 +0530

    Updated  ode

--- resources/views/email/ForgetPassword.blade.php
+++ resources/views/email/ForgetPassword.blade.php
@@ -1,4 +1,4 @@
 Click here To Reset Your Password:<br>
-<a href="http://dev-portal.procric8.com/login.html?token=".{{$token}}>
+<a href="http://dev-portal.procric8.com/new_passwords.html?token={{$token}}">
 	Please Click Here To Reset Password
 </a>
